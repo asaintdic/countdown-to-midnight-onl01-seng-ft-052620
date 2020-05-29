f6deb424ef73b4d1b8ae54ee645992c2c03b2332
Alain Saint-Dic <alains214@gmail.com>
2020-05-29 16:56:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
  
+def countdown_with_sleep(number)
+  number = 10 
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep 1 
+  end
+  "HAPPY NEW YEAR!"
+end
+ 
